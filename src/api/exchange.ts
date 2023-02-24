e38640956374c027c28872b52b853885fa44a144
jooyong-boo <qnwndyd159@naver.com>
2023-02-24 20:25:56 +0900

    소수점 2자리 반올림

--- src/api/exchange.ts
+++ src/api/exchange.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const exchange = async (from: string, to: string, amount: number) => {
     try {
         let result = await axios.get(
-            `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`
+            `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}&places=2`
         );
         return result.data.result;
     } catch (err) {
